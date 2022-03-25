b13d71dfe6ba32a2c3e6083e43de4e49ff477a3f
ABK <kasheams6@gmail.com>
2022-03-25 16:58:31 +0600

    small chenge

--- index.php
+++ index.php
@@ -1,21 +1,9 @@
-<!-- /*
-let's introduce proudct,
-me: i have product for sell.
-you: what kind of product?
-me: i have hp monitor.
-you: is it old monitor ?
-me: yes,this is old monitor.
-you : quantity of your product ?
-me: three product,15.6" display.
-you: do you give VGA cable?
-me: yes.
-you: how much do you want for those products?
-me: 8000 taka .
-*/ -->
-
-
 <?php  
+ /*
+Case Study:
 
+	I have three old hp monitors, not new. I want to sell it on my own website. That’s why I should create a product upload form on my website, then upload my product with specification and available quantity. My monitors have a 15.6 inch display. My monitor’s VGA cable status, yes, I will sell it for 8000 tk.
+*/
 $product = "hp monitor" ;  // datatype is string
 
 $product_quantity = 3 ;  // datatype is integer
