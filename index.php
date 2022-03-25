9f665f1b455a19bbbd09495cb0d79fd5fa1d6db4
ABK <kasheams6@gmail.com>
2022-03-25 16:50:28 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!-- /*
+let's introduce proudct,
+me: i have product for sell.
+you: what kind of product?
+me: i have hp monitor.
+you: is it old monitor ?
+me: yes,this is old monitor.
+you : quantity of your product ?
+me: three product,15.6" display.
+you: do you give VGA cable?
+me: yes.
+you: how much do you want for those products?
+me: 8000 taka .
+*/ -->
+
+
+<?php  
+
+$product = "hp monitor" ;  // datatype is string
+
+$product_quantity = 3 ;  // datatype is integer
+
+$product_size = 15.6 ; // datatype is  float
+
+$product_is_old = true ; // datatype is  boolean
+
+$product_price = array("8000","8500" ,"9000"); // datatype is  array
+
+
+
+
